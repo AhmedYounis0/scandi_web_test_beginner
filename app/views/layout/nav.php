1e3a023af9b03383799f85e68f70857d6a8f0b91
younis <correiaftw@gmail.com>
2024-09-06 02:16:58 +0300

    .....

--- app/views/layout/nav.php
+++ app/views/layout/nav.php
@@ -3,9 +3,9 @@
         <h1 class="navbar-brand">Product List</h1>
         <span class="d-flex">
             <?php if ($title == 'Products') : ?>
-            <a href="/product/create" class="btn btn-dark m-2" type="submit">Add</a>
+            <a href="/product/create" class="btn btn-dark m-2" type="submit">ADD</a>
 <!--            <form method="post" id="delete-form">-->
-                <button class="btn btn-dark m-2" id="delete-product-btn" type="submit">Mass Delete</button>
+                <button class="btn btn-dark m-2" id="delete-product-btn" type="submit">MASS DELETE</button>
 <!--            </form>-->
             <?php endif; ?>
         </span>
